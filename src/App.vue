72813c43e681063e993c239327d33c7199b912a2
rich4-32ras <semyon.shilo@yandex.ru>
2023-03-29 20:19:19 +0300

    hw2 fix

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,22 @@
-
 <script setup>
-  import Cord from './components/Cord/Cord.vue';
-import Counter from './components/Counter/Counter.vue';
-  import Time from './components/Time/Time.vue';
+import css from './App.module.css'
+import { ref } from 'vue';
+
+const  text = ref("");
+const cards = ref([{msg: "деревня", inf: "за городом"}, {msg: "деревяшки", inf: "из них строят дома"}, {msg: "дом", inf: "можно жить и есть"}]);
+
 </script>
 
 <template>
-  <h1>Настоящее время</h1>
-  <Time></Time>
-  <Counter></Counter>
-  <Cord></Cord>
+    <h1 :class="css.myClass" >//HW2//</h1>
+    <input :class="css.myInput" v-model="text"/>
+
+    <template  v-for="card in cards">
+      
+      <div :class="css.myRectangleCard" v-if="card.msg.includes(text)">
+        <h2>{{ card.msg }}</h2>
+        <p>{{ card.inf }}</p>
+      </div>
+    
+    </template>
 </template>
-
-<style scoped>
-h1 {
-  color: rgb(13, 13, 14);
-  text-align: center;  
-}
-
-</style>
